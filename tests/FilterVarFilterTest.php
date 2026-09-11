69a6f5e14f1fa925d5d0d9f42e46fecb009d5b8b
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-09-11 02:58:23 +0200

    test: migrate FilterVar regression to Pest

--- tests/FilterVarFilterTest.php
+++ tests/FilterVarFilterTest.php
@@ -2,19 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Componenta\Filter\Tests;
-
 use Componenta\Filter\FilterVarFilter;
-use PHPUnit\Framework\TestCase;
 
-final class FilterVarFilterTest extends TestCase
-{
-    public function testBooleanValidationDistinguishesFalseFromFailure(): void
-    {
-        $filter = new FilterVarFilter(FILTER_VALIDATE_BOOLEAN);
+it('distinguishes a valid false boolean from validation failure', function (): void {
+    $filter = new FilterVarFilter(FILTER_VALIDATE_BOOLEAN);
 
-        self::assertTrue($filter->accept(false));
-        self::assertTrue($filter->accept('false'));
-        self::assertFalse($filter->accept('not-a-boolean'));
-    }
-}
+    expect($filter->accept(false))->toBeTrue()
+        ->and($filter->accept('false'))->toBeTrue()
+        ->and($filter->accept('not-a-boolean'))->toBeFalse();
+});
