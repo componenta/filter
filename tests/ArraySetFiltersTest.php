22be283839d53c353b131e3af25919aeda346918
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-09-11 03:16:17 +0200

    test: reproduce nested array set comparison failures

---
+++ tests/ArraySetFiltersTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Componenta\Filter\ArrayDiffFilter;
+use Componenta\Filter\ArrayIntersectFilter;
+
+it('does not collapse distinct nested arrays into the same value', function (): void {
+    expect((new ArrayIntersectFilter([[2]]))->accept([[1]]))->toBeFalse()
+        ->and((new ArrayDiffFilter([[2]]))->accept([[1]]))->toBeTrue();
+});
+
+it('compares non-stringable objects by identity without throwing', function (): void {
+    $first = new stdClass();
+    $second = new stdClass();
+
+    expect((new ArrayIntersectFilter([$first]))->accept([$first]))->toBeTrue()
+        ->and((new ArrayIntersectFilter([$first]))->accept([$second]))->toBeFalse()
+        ->and((new ArrayDiffFilter([$first]))->accept([$first]))->toBeFalse()
+        ->and((new ArrayDiffFilter([$first]))->accept([$second]))->toBeTrue();
+});
+
+it('retains scalar string-comparison compatibility', function (): void {
+    expect((new ArrayIntersectFilter([1]))->accept(['1']))->toBeTrue()
+        ->and((new ArrayDiffFilter([1]))->accept(['1']))->toBeFalse();
+});
