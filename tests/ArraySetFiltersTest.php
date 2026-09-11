258afaf13851f80c4e17ebc61c1ad2f691238e9f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-09-11 03:25:42 +0200

    test: reproduce recursive array comparison failure

--- tests/ArraySetFiltersTest.php
+++ tests/ArraySetFiltersTest.php
@@ -20,6 +20,17 @@ it('compares non-stringable objects by identity without throwing', function (): 
         ->and((new ArrayDiffFilter([$first]))->accept([$second]))->toBeTrue();
 });
 
+it('does not throw while comparing recursive arrays', function (): void {
+    $first = [];
+    $first['self'] = &$first;
+
+    $second = [];
+    $second['self'] = &$second;
+
+    expect((new ArrayIntersectFilter([$first]))->accept([$second]))->toBeFalse()
+        ->and((new ArrayDiffFilter([$first]))->accept([$second]))->toBeTrue();
+});
+
 it('retains scalar string-comparison compatibility', function (): void {
     expect((new ArrayIntersectFilter([1]))->accept(['1']))->toBeTrue()
         ->and((new ArrayDiffFilter([1]))->accept(['1']))->toBeFalse();
