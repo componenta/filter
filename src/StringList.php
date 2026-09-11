4e9a0955e6686ebea8c11d8c498573e0c3f84d9f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-09-11 03:20:03 +0200

    fix: add string list configuration validation

---
+++ src/StringList.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Componenta\Filter;
+
+/** @internal */
+final class StringList
+{
+    private function __construct()
+    {
+    }
+
+    public static function assert(array $values, string $argument): void
+    {
+        foreach ($values as $key => $value) {
+            if (!is_string($value)) {
+                throw new \InvalidArgumentException(sprintf(
+                    '%s[%s] must be a string, %s given',
+                    $argument,
+                    (string) $key,
+                    get_debug_type($value),
+                ));
+            }
+        }
+    }
+}
