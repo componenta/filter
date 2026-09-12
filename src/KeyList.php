30a111f96ad056d159764e55a695bfc0fc735e34
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-09-12 12:41:23 +0200

    refactor: remove redundant key-list index cast

--- src/KeyList.php
+++ src/KeyList.php
@@ -18,7 +18,7 @@ final class KeyList
                 throw new \InvalidArgumentException(sprintf(
                     '%s[%s] must be string, int, or null; %s given',
                     $argument,
-                    (string) $index,
+                    $index,
                     get_debug_type($value),
                 ));
             }
